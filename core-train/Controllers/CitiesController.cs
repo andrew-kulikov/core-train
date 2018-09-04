bb6c16687cbb2e3f12601e365eb8fdc4b57ff371
Andrew <andrew.1.kulikov@gmail.com>
2018-09-04 22:56:47 +0300

    Create basic controller

---
+++ core-train/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace coreTrain.Controllers
+{
+	[Route("api/cities")]
+	public class CitiesController : Controller
+	{
+		[HttpGet]
+		public JsonResult Index()
+		{		
+			return new JsonResult(new List<object>
+			{
+				new { id = 1, Name = "NY" },
+				new { id = 2, Name = "LA" }
+			});
+		}
+	}
+}
